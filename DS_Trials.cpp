1c8aec47dbe006502f91934333417a4488acc63d
Suee18 <121303661+Suee18@users.noreply.github.com>
2023-11-09 14:53:06 +0200

    Update DS_Trials.cpp

--- DS_Trials.cpp
+++ DS_Trials.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
 using namespace std;
 
 class Account {
 public:
-    int account_number;
+    int accountNumber;
+    int pin;
     string account_holder_name;
     double balance;
     Account* next;
 
     void display() {
-        cout << "Account number: " << account_number << endl;
-        cout << "Account holder name: " << account_holder_name << endl;
-        cout << "Balance: " << balance << endl;
+        cout << "Account number: [" << accountNumber << "]\n";
+        cout << "Account holder name: [" << account_holder_name << "]\n";
+        cout << "Balance: [" << balance << "]\n";
     }
 
 };
@@ -20,22 +22,49 @@ public:
 class Bank {
 public:
     Account* head;
+    /*Constructor*/
     Bank() {
         head = NULL;
     }
-    void add_account(int account_number, string account_holder_name, double balance) {
-        Account* new_account = new Account();
-        new_account->account_number = account_number;
+
+
+    void add_account( string account_holder_name, double balance,int pin) {
+        Account* new_account = new Account(); /*new node?*/
+        /*Account number generation*/
+        new_account->accountNumber = rand() % 9000000 + 1000000;
         new_account->account_holder_name = account_holder_name;
         new_account->balance = balance;
+        new_account->pin = pin;
         new_account->next = head;
         head = new_account;
+        cout << "account created succefully!";
     }
+
+
+
+    bool loginVerify(Bank* bank, string account_holder_name, int pin) {
+        Account* currentPtr = bank->head;
+        while (currentPtr != nullptr) {
+            if (currentPtr->account_holder_name == account_holder_name) {
+                if (currentPtr->pin == pin) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            currentPtr = currentPtr->next;
+        }
+        return false;
+    }
+
+
+
     void delete_account(int account_number) {
         Account* current = head;
         Account* previous = NULL;
         while (current != NULL) {
-            if (current->account_number == account_number) {
+            if (current->accountNumber == account_number) {
                 if (previous == NULL) {
                     head = current->next;
                 }
@@ -52,7 +81,7 @@ public:
     void deposit(int account_number, double amount) {
         Account* current = head;
         while (current != NULL) {
-            if (current->account_number == account_number) {
+            if (current->accountNumber == account_number) {
                 current->balance += amount;
                 return;
             }
@@ -62,7 +91,7 @@ public:
     void withdraw(int account_number, double amount) {
         Account* current = head;
         while (current != NULL) {
-            if (current->account_number == account_number) {
+            if (current->accountNumber == account_number) {
                 if (current->balance >= amount) {
                     current->balance -= amount;
                 }
@@ -77,7 +106,7 @@ public:
     void balance_inquiry(int account_number) {
         Account* current = head;
         while (current != NULL) {
-            if (current->account_number == account_number) {
+            if (current->accountNumber == account_number) {
                 cout << "Account holder name: " << current->account_holder_name << endl;
                 cout << "Balance: " << current->balance << endl;
                 return;
@@ -88,7 +117,63 @@ public:
 };
 
 int main() {
-    Bank bank;
+    Bank user;
+    short choice1,choice2,count;
+    int pin;
+    string userName;
+
+    cout << "Press\n (1)Sign in.\t(2)Sign up.\n ";
+    cin >> choice1;
+    if (choice1 == 1) {
+        /*checks for sign in info function make it boolean*/
+        cout << "Enter your name";
+        cin >> userName;
+        cout << "Enter the pin";
+        cin >> pin;
+
+        if (user.loginVerify(&user,userName,pin)==true) 
+        { /*check the pass by reference and the parameter concept!!!!*/
+            /*enters to main menu #2*/ 
+            cout << "Press a number for transactions:\n (1)View balance\n(2)Deposit\n(3)Withdraw\n(4)DeleteAccount\n";
+            cin >> choice2;
+
+            if (choice2 == 1) {}
+            else if (choice2 == 2) {}
+            else if (choice2 == 3) {}
+            else if (choice2 == 4) {}
+            else cerr << "Invalid Input";
+            return;
+        }
+
+
+
+        else cerr << "Invalid username or password.";/*returns to log in n numbers of times*/
+        count++;
+        if (count >5)
+            return;
+        else cerr << "Exceeded number of Logging in mistakes";
+    }
+
+
+    else if (choice1 == 2) {
+        /*call add account fn*/
+      
+    }
+    else cerr << "Invalid input.";
+    /*return to main menu*/
+    
+
+
+
+
+
+
+
+
+
+
+
+   /* Bank bank;
     bank.add_account(1, "Alice", 1000);
     bank.add_account(2, "Bob", 2000);
     bank.add_account(3, "Charlie", 3000);
@@ -96,6 +181,7 @@ int main() {
     bank.withdraw(2, 1000);
     bank.balance_inquiry(3);
     bank.delete_account(2);
-    bank.head->display();
+    bank.head->display();*/
     return 0;
 }
+
