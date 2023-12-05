9a6a767c6709426c946da20f5318dccbe9f92147
mohamedelsrougy <150448836+mohamedelsrougy@users.noreply.github.com>
2023-12-05 23:21:15 +0200

    Update bank.h

--- bank.h
+++ bank.h
@@ -4,21 +4,29 @@
 #include <string>
 #include<fstream>
 #include <string>
+#include "cryptopp/aes.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/filters.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/files.h"
+#include <sstream>
+
 using namespace std;
 
 class Bank
 {
 
-    public:
-     class Account
-     {
-       protected:
+public:
+    class Account
+    {
+    protected:
         int pin;
 
-       public:
-        unsigned short accountNumber ;
+    public:
+        unsigned short accountNumber;
         string accounOwnerName;
-        double balance ;
+        double balance;
         Account* next;
 
 
@@ -29,28 +37,26 @@ class Bank
 
     };
 
-    
-    public:
-         //Bank data membersss
-        unsigned short count = 0;
+
+public:
+    //Bank data membersss
+    unsigned short count = 0;
 public:
     typedef Account* AccountPointer;
-        AccountPointer head;
+    AccountPointer head;
 
 
     Bank();
     unsigned short AccountsNum();
     void addAccount(string accounOwnerName, double balance, int pin);
     bool AccNumSearch(unsigned short accountNumber);
-    bool loginVerify(Bank * bank, string accounOwnerName, int pin);
+    bool loginVerify(Bank* bank, string accounOwnerName, int pin);
     void deleteAccount(int accountNum);
     void deposit(int accountNum, double amount);
     void withdraw(int accountNum, double amount);
-    unsigned short findAccount(const string & username, int pin);
-    void BalanceDisplay(const string & account_holder_name);
+    unsigned short findAccount(const string& username, int pin);
+    void BalanceDisplay(const string& account_holder_name);
     void TransferMoney(unsigned short AccountNum1, unsigned short AccountNum, double Amount);
-    void Write_List_into_Files();
-    void Read_File_into_List();
-
+    void EncryptAndWriteToFile();
+    void ReadFileAndDecrypt();
 };
-
