4673647a46ff506cc7b91bb2282932ce3ca49228
Suee18 <121303661+Suee18@users.noreply.github.com>
2023-12-06 03:31:59 +0200

    Update bank.h

--- bank.h
+++ bank.h
@@ -1,18 +1,27 @@
 #pragma once
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include<fstream>
 #include <string>
-#include "cryptopp/aes.h"
-#include "cryptopp/modes.h"
-#include "cryptopp/filters.h"
-#include "cryptopp/osrng.h"
-#include "cryptopp/hex.h"
-#include "cryptopp/files.h"
+#include "aes.h"
+#include "modes.h"
+#include "filters.h"
+#include "files.h"
+#include "osrng.h"
+#include "hex.h"
+#include <sstream>
+#include "cryptlib.h"
+#include "rijndael.h"
 #include <sstream>
 
+using namespace CryptoPP;
 using namespace std;
+
+
+using namespace std;
+
 
 class Bank
 {
@@ -28,6 +37,10 @@ public:
         string accounOwnerName;
         double balance;
         Account* next;
+        //constructor
+        Account();
+        Account(const string& ownerName, unsigned short accNumber, double accBalance, int accPin);
+            
 
 
         int getPin();
@@ -41,9 +54,14 @@ public:
 public:
     //Bank data membersss
     unsigned short count = 0;
+    AutoSeededRandomPool prng;  // Move prng to the class level
+
 public:
     typedef Account* AccountPointer;
     AccountPointer head;
+    // Add member variables for key and IV in your Bank class
+    SecByteBlock encryptionKey;
+    SecByteBlock encryptionIV;
 
 
     Bank();
@@ -57,6 +75,6 @@ public:
     unsigned short findAccount(const string& username, int pin);
     void BalanceDisplay(const string& account_holder_name);
     void TransferMoney(unsigned short AccountNum1, unsigned short AccountNum, double Amount);
-    void EncryptAndWriteToFile();
-    void ReadFileAndDecrypt();
+    void EncryptAndWriteToFile(AutoSeededRandomPool& prng);
+    void DecryptFromFile(AutoSeededRandomPool& prng) ;
 };
